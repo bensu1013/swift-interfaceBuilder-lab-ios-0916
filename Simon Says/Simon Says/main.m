7a66fbd611ae3b702b05acdcde7c0bee188d116e
RWoolard <r.woolard456@gmail.com>
2016-08-31 12:43:41 -0400

    Done

---
+++ Simon Says/Simon Says/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Simon Says
+//
+//  Created by Flatiron School on 8/31/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
